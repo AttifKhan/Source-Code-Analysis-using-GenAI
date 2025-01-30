5f75872956da7b6629a722afa5590d6b82ae1510
Attif Khan <127493496+AttifKhan@users.noreply.github.com>
2025-01-30 18:10:18 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import find_packages, setup
 setup(
     name = 'Generative AI Project',
     version= '0.0.0',
-    author= 'FARAZ ALI KHAN',
-    author_email= '7411046755khan@gmail.com',
+    author= 'ATTIF KHAN',
+    author_email= 'kk1527218@gmail.com',
     packages= find_packages(),
     install_requires = []
 
